3cb639c96712c46fff4bd6ae8ca1c1234aafc198
rijalislamir <rijalislamir@gmail.com>
2022-06-28 08:16:34 +0700

    Initial commit

---
+++ RELAY/RELAY.ino
@@ -0,0 +1,20 @@
+const int RELAY1_PIN = 27;
+const int RELAY2_PIN = 5;
+
+unsigned long start_time = 0;
+
+void setup() {
+  pinMode(RELAY1_PIN, OUTPUT);
+  pinMode(RELAY2_PIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  start_time = millis();
+  digitalWrite(RELAY1_PIN, LOW);  
+  digitalWrite(RELAY2_PIN, LOW);     
+  delay(5000);                      
+  digitalWrite(RELAY1_PIN, HIGH); 
+  digitalWrite(RELAY2_PIN, HIGH);  
+  delay(3000);                      
+}
